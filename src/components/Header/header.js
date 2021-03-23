ea1b4288f8ed058d2e67e5a347004d666094395a
Robin Khiv <robinkhiv@gmail.com>
2021-03-22 21:01:53 -0700

    spelling fix

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -3,7 +3,7 @@ import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 function Header(){
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Brand href="#home">Orange Blossom Nusery</Navbar.Brand>
+            <Navbar.Brand href="#home">Orange Blossom Nursery</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto">
