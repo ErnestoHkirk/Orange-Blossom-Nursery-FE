5f87fd4aa29069b06e8cf1ddf005ad7fbdb43bef
Robin Khiv <robinkhiv@gmail.com>
2021-04-26 21:59:46 -0700

    added context api

--- src/Routes/dashboard.js
+++ src/Routes/dashboard.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+//import VenderService from '../../../ob332be/src/routes/vendor/vender-service';
+import { PlantProvider } from '../context/PlantContext';
+import { VendorProvider } from '../context/VendorContext';
 
 export default function dashboard() {
     return (
-        <div>
-            <Link to="/">New Order</Link>
-            <Link to="/">Vendor Order</Link>
-            <Link to="/">Delivery Form</Link>
-            <Link to="/">Pending Order</Link>
-            <Link to="/">Statement</Link>
-        </div>
+        <VendorProvider>
+            <PlantProvider>
+                <Link to="/">New Order</Link>
+                <Link to="/">Vendor Order</Link>
+                <Link to="/">Delivery Form</Link>
+                <Link to="/">Pending Order</Link>
+                <Link to="/">Statement</Link>
+            </PlantProvider>
+        </VendorProvider>
     )
 }
